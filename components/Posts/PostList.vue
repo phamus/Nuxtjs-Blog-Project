dda997934bfe128994b1096f61dbe36677aad509
phamus <sphamus@gmail.com>
2020-06-13 22:10:55 +0100

    admin routes

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,12 +2,14 @@
   <section class="posts-list">
     <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://www.liberaldictionary.com/wp-content/uploads/2018/11/tech.jpg"
       postTitle="Post 1"
       postText="My First Post"
     />
     <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://www.vapulus.com/en/wp-content/uploads/2019/01/tech.jpg"
       postTitle="Post 2"
       postText="My Second Post"
@@ -20,6 +22,12 @@ import PostPreview from "~/components/Posts/PostPreview";
 export default {
   components: {
     PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
   }
 };
 </script>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "~/components/Admin/AdminPostForm";
+export default {
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: "Phamus",
+        title: "Skype",
+        thumbnailLink:
+          "https://www.liberaldictionary.com/wp-content/uploads/2018/11/tech.jpg",
+        content: "Nice post"
+      }
+    };
+  }
+};
+</script>
